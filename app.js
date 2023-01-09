4fd6314dde3633a1b05373d8243d6e19b7469ea4
Shashank-Redapple <SHASHANK@REDAPPLETECH.COM>
2023-01-09 16:29:52 +0530

    Swagger UI implemented

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ const express= require("express");
 const mongoose=require("mongoose");
 const app=express();
 const dotenv=require("dotenv");
+// Swagger related
+const swaggerUi = require('swagger-ui-express');
+const YAML=require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 const TaskRoutes=require("./routes/taskRoute");
 const errorHandlerMiddleware=require("./middleware/errorHandlerMiddleware");
 dotenv.config({path:"./.env"});
 let port=process.env.PORT;
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 app.use("/api/v1",TaskRoutes);
 mongoose.set('strictQuery', false);
+
+app.get('/',(req,res)=>{
+    res.send('hi swagger api ');
+})
 
 mongoose.connect(process.env.MONGO_URI,()=>{
     console.log("DB Connected!!!");
